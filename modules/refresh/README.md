dae3d96413ad92002469a155fc4244909e27a5dc
xwenliang <wenliang.web@gmail.com>
2015-03-04 16:00:26 +0800

    update refresh

---
+++ modules/refresh/README.md
@@ -0,0 +1,51 @@
+##基于iScroll5的上/下拉刷新
+
+一个上/下拉刷新组件通常包含如下结构：
+
+	<div class="scroller-wrap">
+		<div class="scroller">
+			<div class="scroller-content">
+				content
+			</div>
+			<div class="down">
+				pulldown tips
+			</div>
+			<div class="up">
+				pullup tips
+			</div>
+		</div>
+	</div>
+	
+####options.container
+上/下拉刷新的外容器，即上面的`scroller-wrap`
+
+它是需要有固定高度的
+
+默认值：无
+
+
+###options.distance
+上/下拉刷新的触发距离，通常它就是上面down/up的高度
+
+默认值 `50`
+
+###options.pullUpAction
+上拉到临界值触发的函数，通常它里面会触发一个ajax请求来更新数据
+
+默认值 `null`
+
+###options.pullDownAction
+下拉到临界值触发的函数，通常它里面会触发一个ajax请求来更新数据
+
+默认值 `null`
+
+###options.probeType
+scroll事件触发的时候，其回调的执行频率，共分为1，2，3三个值
+
+值越大，执行频率越高
+
+默认值 `3`
+
+<br>
+<br>
+详情见：[iScroll5文档](http://iiunknown.gitbooks.io/iscroll-5-api-cn/content/index.html)
--- modules/refresh/refresh.js
+++ modules/refresh/refresh.js
@@ -1,13 +1,12 @@
 /*
- * @author zooble
  * @date 2015-2-5
  * @desc 下/上拉刷新
  * @params
  * @param container(str)			容器
  * @param distance(num)				临界值
- * @parem topOffset(num)			刷新提示文案的高度
  * @param pullUpAction(fn)			上拉触发的函数
  * @param pullDownAction(fn)		下拉触发的函数
+ * @param probeType(num)			scroll事件触发中探针的活跃度
  */
 var $ = require('Zepto');
 var iscroll = require('iScroll');
@@ -15,9 +14,9 @@ var iscroll = require('iScroll');
 var config = {
 	container: '',
 	distance: 50,
-	topOffset: 40,
 	pullUpAction: null,
-	pullDownAction: null
+	pullDownAction: null,
+	probeType: 3
 };
 function zRefresh(opt){
 	if(!(this instanceof zRefresh)){
@@ -31,8 +30,8 @@ zRefresh.prototype = {
 	init: function(){
 		var me = this;
 		var myscroll = me.myscroll = new iscroll(me.opt.container, {
-			probeType: 3,
-			topOffset: -me.opt.topOffset
+			probeType: me.opt.probeType,
+			topOffset: -me.opt.distance
 		});
 		myscroll.canExec = true;
 		myscroll.on('scrollStart', function(){
@@ -59,7 +58,3 @@ zRefresh.prototype = {
 };
 
 module.exports = zRefresh;
-
-
-
-
