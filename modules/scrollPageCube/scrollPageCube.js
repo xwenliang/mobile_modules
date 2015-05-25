aa82c7de1d00f8d44a66bcc9a5c36d25d0bc0cc7
xwenliang <wenliang.web@gmail.com>
2015-05-26 01:36:56 +0800

    record

--- modules/scrollPageCube/scrollPageCube.js
+++ modules/scrollPageCube/scrollPageCube.js
@@ -5,7 +5,12 @@
 
 var $ = require('Zepto');
 
-var opt = {
+var options = {
+	par: '.scroll-page-cube-perspective',
+	box: '.scroll-page-cube-box',
+	item: '.scroll-page-cube-item',
+	left: '.scroll-page-cube-left',
+	right: '.scroll-page-cube-right',
 	bonus: 50//垂直移动距离超过该值，就触发滚动
 };
 function scrollPageCube(config){
@@ -16,17 +21,119 @@ function scrollPageCube(config){
 };
 scrollPageCube.prototype = {
 	constructor: scrollPageCube,
+
 	init: function(config){
-		
+		var opt = this.opt = $.extend({}, options, config);
+		opt.$par = $(opt.par);
+		opt.$box = opt.$par.find(opt.box);
+		opt.$item = opt.$par.find(opt.item);
+		opt.$left = opt.$par.find(opt.left);
+		opt.$right = opt.$par.find(opt.right);
+		//构建一个3d盒子，盒子的两个侧面应该是正方形
+		opt.size = this.getSize();
+		opt.$box.css({
+			'-webkit-transform': 'translateZ(-' + opt.size.h/2 + 'px)'
+		});
+		opt.$left.css({
+			'width': opt.size.h + 'px',
+			'left': (opt.size.w - opt.size.h)/2 + 'px',
+			'-webkit-transform': 'rotateY(-90deg) translateZ('+ opt.size.w/2 +'px)'//旋转的时候，坐标轴也旋转了
+		});
+		opt.$right.css({
+			'width': opt.size.h + 'px',
+			'left': (opt.size.w - opt.size.h)/2 + 'px',
+			'-webkit-transform': 'rotateY(90deg) translateZ('+ opt.size.w/2 +'px)'
+		});
+		opt.$item.eq(0).css({
+			'-webkit-transform': 'translateZ(' + opt.size.h/2 + 'px)'
+		});
+		opt.curIndex = 0;
+		this.beforeAction();
+		this.followFinger();
+		this.preventDefault();
+	},
+	getSize: function(){
+		var size = {
+			w: this.opt.$par.width(),
+			h: this.opt.$par.height()
+		};
+		if(!size.h){
+			console.log('Warn: check the height of container');
+		}
+		return size;
 	},
 	beforeAction: function(){
-
-	},
-	doAction: function(){
-
+		var opt = this.opt;
+		if(opt.curIndex < opt.$item.length - 1){
+			opt.$item.eq(opt.curIndex+1).css({
+				'-webkit-transform': 'rotateX(-90deg) translateZ(' + opt.size.h/2 + 'px)'
+			});
+		}
 	},
 	afterAction: function(){
+		this.opt.curIndex++;
+	},
+	followFinger: function(){
+		var opt = this.opt;
+		var me = this;
+		opt.$par.on({
+			touchstart: function(e){
+				opt.$box.removeClass('scroll-page-cube-tweening');
+				me.beforeAction();
+				this.movestart = {
+					x: e.touches[0].pageX,
+					y: e.touches[0].pageY
+				};
+			},
+			touchmove: function(e){
+				this.moving = {
+					x: e.touches[0].pageX - this.movestart.x,
+					y: e.touches[0].pageY - this.movestart.y
+				};
+				var trangle = {
+					x: -(this.moving.y/opt.size.h)*90 + 'deg',
+					y: (this.moving.x/opt.size.w)*30 + 'deg'//y轴要限制一下
+				};
+				//第一页
+				if(opt.curIndex === 0 && this.moving.y > 0){
+					trangle.x = 0;
+				}
+				//最后一页
+				else if(opt.curIndex === opt.$item.length && this.moving.y < 0){
+					trangle.x = 0;
+				}
 
+				opt.$box.css(
+					'-webkit-transform', 'translateZ(-' + opt.size.h/2 + 'px) '
+										+'rotateX('+ trangle.x +') '
+										+'rotateY('+ trangle.y +')'
+				);
+			},
+			touchend: function(e){
+				var moved = this.moving;
+				opt.$box.addClass('scroll-page-cube-tweening');
+				//未达到触发条件
+				// if(Math.abs(moved.y) < opt.bonus){
+
+				// }
+				// else{
+
+				// }
+				opt.$box.css(
+					'-webkit-transform', 'translateZ(-' + opt.size.h/2 + 'px) '
+										+'rotateX(90deg) '
+										+'rotateY(0)'
+				);
+				me.afterAction();
+			}
+		});
+	},
+	preventDefault: function(){
+		$(document).on('touchstart', function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		});
 	}
 };
 
